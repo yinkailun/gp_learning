2531971a1c8645ed86d37ebd74f44c3ea29badcd
Yinkl <yin.kl@ikcrm.com>
2019-06-27 15:03:52 +0800

    init

---
+++ gplearning/src/main/java/io/bio/mytomcat/MyTomcat.java
@@ -0,0 +1,22 @@
+package io.bio.mytomcat;
+
+/**
+ * @author yinkailun
+ * @description:
+ * @date 2019-06-15 9:12 PM
+ */
+public class MyTomcat {
+	//J2EE标准
+	//Servlet
+	//Request
+	//Response
+
+	//配置启动端口，8080，serverSocket,IP:localhost
+	//配置web.xml,servlet继承HttpServlet
+	//配置servlet-name
+	//servlet-class
+	//url-pattern
+
+
+
+}
---
+++ gplearning/src/main/java/netty/io/IO.java
@@ -0,0 +1,17 @@
+package netty.io;
+
+import java.nio.ByteBuffer;
+import java.nio.channels.Channel;
+
+/**
+ * @author yinkailun
+ * @description:
+ * @date 2019-06-15 4:18 PM
+ */
+public class IO {
+	public static void main(String[] args) {
+		ByteBuffer bf = ByteBuffer.allocate(10) ;
+//		Channel
+	}
+
+}
